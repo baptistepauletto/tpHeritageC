b7cabef30b98d6d8d70394b323789fdfab64f471
baptiste-pauletto1 <baptistep.contact@gmail.com>
2018-11-19 18:21:42 +0100

    Version temporaire de travail & modification du schéma UML

--- Catalogue.h
+++ Catalogue.h
@@ -1,14 +1,14 @@
 /*************************************************************************
-                           Xxx  -  description
+                           Catalogue  -  description
                              -------------------
     début                : $DATE$
     copyright            : (C) $YEAR$ par $AUTHOR$
     e-mail               : $EMAIL$
 *************************************************************************/
 
-//---------- Interface de la classe <Xxx> (fichier Xxx.h) ----------------
-#if ! defined ( XXX_H )
-#define XXX_H
+//---------- Interface de la classe <Catalogue> (fichier Catalogue.h) ----------------
+#if ! defined ( CATALOGUE_H )
+#define CATALOGUE_H
 
 //--------------------------------------------------- Interfaces utilisées
 
@@ -17,46 +17,55 @@
 //------------------------------------------------------------------ Types
 
 //------------------------------------------------------------------------
-// Rôle de la classe <Xxx>
-//
-//
+// Rôle de la classe <Catalogue>
+// La classe catalogue a pour objectif de recenser l'ensemble des  trajets
+// dans l'attribut : trajetsDisponibles,
+// ainsi que de proposer des parcours pour deux paramètres donnés suivants
+// - villeDepart : ville de départ du voyage demandé
+// - villeFin : ville où se termine le voyage
+// On peut enfin ajouter des trajets ainsi qu'afficher l'état du catalogue
 //------------------------------------------------------------------------
 
-class Xxx : public Ancetre
+class Catalogue
 {
 //----------------------------------------------------------------- PUBLIC
 
 public:
 //----------------------------------------------------- Méthodes publiques
-    // type Méthode ( liste des paramètres );
-    // Mode d'emploi :
-    //
-    // Contrat :
+    void Afficher() const
+    // Mode d'emploi : Nous affichons de manière ordonée le contenu du 
+    // catalogue à l'instant T, où la fonction est appellée 
+    // Contrat : Elle ne modifiera pas le contenu de l'instance catalogue
+
+    void AjouterTrajet(const Trajet & t) 
+    // Mode d'emploi : Le trajet passé en paramètre, s'il est différent
+    // de ceux déjà présents, sera ajouté au catalogue via un placement
+    // au premier index libre via la méthode Ajouter de CollectionTrajet.
+    // Contrat : Le trajet doit exister, bien entendu.
     //
 
-
-//------------------------------------------------- Surcharge d'opérateurs
-    Xxx & operator = ( const Xxx & unXxx );
-    // Mode d'emploi :
-    //
-    // Contrat :
-    //
-
+    void rechercherParcours(const std::string & vDep,
+			    const std::string & vFin) const
+    // Mode d'emploi : Nous récupérons dans la collection de trajets,
+    // ceux qui possèdent une ville de départ ET une ville d'arrivée
+    // égales aux paramètres, nous les affichons alors.
+    // Contrat : vDep (et vFin) appartiennent à l'ensemble des villes
+    // de départ (et fin, respectivement) dans le catalogue.
 
 //-------------------------------------------- Constructeurs - destructeur
-    Xxx ( const Xxx & unXxx );
+    //Catalogue ( const std::string & Catalogue );
     // Mode d'emploi (constructeur de copie) :
     //
     // Contrat :
     //
 
-    Xxx ( );
+    Catalogue ( );
     // Mode d'emploi :
     //
     // Contrat :
     //
 
-    virtual ~Xxx ( );
+    virtual ~Catalogue ( );
     // Mode d'emploi :
     //
     // Contrat :
@@ -68,10 +77,10 @@ protected:
 //----------------------------------------------------- Méthodes protégées
 
 //----------------------------------------------------- Attributs protégés
-
+	CollectionTrajet trajetsDisponibles; 
 };
 
-//-------------------------------- Autres définitions dépendantes de <Xxx>
+//-------------------------------- Autres définitions dépendantes de <Catalogue>
 
-#endif // XXX_H
+#endif // CATALOGUE_H
 
