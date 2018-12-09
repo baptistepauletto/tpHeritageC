7b3f10e312b7e85dfad409745531d0aa972de1e8
baptiste-pauletto1 <baptistep.contact@gmail.com>
2018-12-09 14:29:45 +0100

    Amélioration du main étape 1

--- main.cpp
+++ main.cpp
@@ -18,7 +18,35 @@ int main(){
 	
 	TrajetSimple trajetInit(villeDepartInit,villeArriveeInit,moyenDeTransportInit);
 	Catalogue catalogue(trajetInit);
-
+	
+	char * action = new char[50];
+	cin >> action;	
+	while(strcmp(action,"quit") != 0){
+		if(strcmp(action,"insert") == 0){
+			cout<<"rentré insert" << endl;
+			cin >> action;
+			if(strcmp(action,"TS") == 0){
+				cout<< "rentré T" << endl;
+				cin >> action;
+				strcpy(villeDepartInit,action);
+				cin >> action;
+				strcpy(villeArriveeInit,action);
+				cin >> action;
+				strcpy(moyenDeTransportInit,action);
+				static TrajetSimple trajetS(villeDepartInit,villeArriveeInit,moyenDeTransportInit);
+				catalogue.AjouterTrajet(trajetS);
+			}
+			if(strcmp(action,"TC") == 0){
+				cout << "rentré TS"<<endl;
+			}
+		}
+		if(strcmp(action,"afficher") == 0){
+			catalogue.Afficher();
+		}
+		cin >> action;
+	}
+	
+	delete [] action;	
 	delete [] villeDepartInit;	
 	delete [] villeArriveeInit;	
 	delete [] moyenDeTransportInit;	
