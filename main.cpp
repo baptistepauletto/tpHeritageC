202bd0ba4178b00078ab222d82f9a0dedd80d733
baptiste-pauletto1 <baptistep.contact@gmail.com>
2018-12-09 15:40:10 +0100

    Amélioration du main V3

--- main.cpp
+++ main.cpp
@@ -14,41 +14,66 @@ int main(){
 	char * moyenDeTransportInit = new char[20];
 	strcpy(villeDepartInit,"Lyon");
 	strcpy(villeArriveeInit,"Avignon");
-	strcpy(moyenDeTransportInit,"Vélo");
+	strcpy(moyenDeTransportInit,"TGV");
 	
 	TrajetSimple trajetInit(villeDepartInit,villeArriveeInit,moyenDeTransportInit);
 	Catalogue catalogue(trajetInit);
-	
+	cout << "Bienvenue dans notre mini application de gestion de trajet ! \r\n"
+	     << "Si vous souhaitez consulter la liste des commandes, tapez help." << endl;
 	char * action = new char[50];
-	TrajetSimple * trajetS = nullptr;
+	TrajetSimple * adresses[50];
+	unsigned cptTrajetsSimples = 0;
+	//TrajetSimple * trajetS = nullptr;
+	//TrajetCompose * trajetC = nullptr;
 	cin >> action;	
 	while(strcmp(action,"quit") != 0){
 		if(strcmp(action,"insert") == 0){
 			cout<<"rentré insert" << endl;
 			cin >> action;
 			if(strcmp(action,"TS") == 0){
-				cout<< "rentré T" << endl;
+				cout<< "rentré TS" << endl;
+				//ajouterTrajetSimple(villeDepartInit,villeArriveeInit,moyenDeTransportInit,action,trajetS,catalogue);
 				cin >> action;
 				strcpy(villeDepartInit,action);
 				cin >> action;
 				strcpy(villeArriveeInit,action);
 				cin >> action;
 				strcpy(moyenDeTransportInit,action);
-				trajetS = new TrajetSimple(villeDepartInit,villeArriveeInit,moyenDeTransportInit);
-				catalogue.AjouterTrajet(*trajetS);
+				adresses[cptTrajetsSimples] = new TrajetSimple(villeDepartInit,villeArriveeInit,moyenDeTransportInit);
+				//trajetS = new TrajetSimple(villeDepartInit,villeArriveeInit,moyenDeTransportInit);
+				catalogue.AjouterTrajet(*adresses[cptTrajetsSimples]);
+				cptTrajetsSimples++;
 			}
 			if(strcmp(action,"TC") == 0){
-				cout << "rentré TS"<<endl;
+				cout << "rentré TC"<<endl;
+				//unsigned cptTrajet = 0;
+				//cin >> cptTrajet;
+				//for(unsigned i (0); i < cptTrajet; i++){
+					
+				//}
 			}
 		}
 		if(strcmp(action,"afficher") == 0){
 			catalogue.Afficher();
 		}
+		if(strcmp(action,"help") == 0){
+			cout << "Les commandes disponibles sont : \r\n"
+			     << "Ajouter un trajet simple : insert TS <villeDepart> <villeArrivee> <moyenDeTransport> \r\n"
+			     << "Ajouter un trajet composé : insert TC <n> [<villeDepart> <villeArrivee> <moyenDeTransport>] avec [] n>0 fois \r\n"
+			     << "Afficher l'état actuel du catalogue : afficher \r\n"
+			     << "Effectuer l'algorithme de recherche d'un trajet V1 : recherche V1 <villeDepart> <villeArrivee> \r\n"
+			     << "Effectuer l'algorithme de recherche d'un trajet V2 : recherche V2 <villeDepart> <villeArrivee> \r\n"
+			     << "--Fin de la liste des commandes disponibles-- \r\n";
+		}
 		cin >> action;
 	}
 	
 	delete [] action;
-	delete trajetS;
+	for(unsigned i(0); i<cptTrajetsSimples;i++){
+		delete adresses[i];
+	}
+	//delete trajetS;
+	//delete trajetC;
 	delete [] villeDepartInit;	
 	delete [] villeArriveeInit;	
 	delete [] moyenDeTransportInit;	
